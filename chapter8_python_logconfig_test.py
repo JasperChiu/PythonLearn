cf44f51054c699a34f540dc29fad686c36fc96f4
JasperChiu <Jasper.Chiu@beltom.com.tw>
2022-03-24 15:31:15 +0800

    嘗試使用例外錯誤try-except結合log，將例外錯誤的消息輸出到console並寫成log檔案，檔名Jasper-test-log.log

--- chapter8_python_logconfig_test.py
+++ chapter8_python_logconfig_test.py
@@ -9,8 +9,30 @@ logging.config.fileConfig('logging.conf')
 logger = logging.getLogger('fileAndConsole')
 
 # 'application' code
-logger.debug('debug message')
-logger.info('info message')
-logger.warning('warn message')
-logger.error('error message')
-logger.critical('critical message')
+# logger.debug('debug message')
+# logger.info('info message')
+# logger.warning('warn message')
+# logger.error('error message')
+# logger.critical('critical message')
+
+try:
+    x = 5 / 0
+except:
+    # exc_info=True 參數設為True就可以記錄例外(Exception)
+    logger.debug('debug message', exc_info=True)
+    logger.info('info message', exc_info=True)
+    logger.warning('warn message', exc_info=True)
+    logger.error('error message', exc_info=True)
+    logger.critical('critical message', exc_info=True)
+
+try:
+    x = 1
+    y = "1"
+    z = x + y
+except:
+    # exc_info=True 參數設為True就可以記錄例外(Exception)
+    logger.debug('debug message', exc_info=True)
+    logger.info('info message', exc_info=True)
+    logger.warning('warn message', exc_info=True)
+    logger.error('error message', exc_info=True)
+    logger.critical('critical message', exc_info=True)
