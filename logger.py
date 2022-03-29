cf5336d2f1266211639d61afe55907767f2321dd
JasperChiu <Jasper.Chiu@beltom.com.tw>
2022-03-29 11:29:30 +0800

    註記logger.py的使用方法

--- logger.py
+++ logger.py
@@ -1,6 +1,16 @@
 import logging
 import os
 from datetime import datetime
+"""
+如何使用logger.py
+首先在要套用本log格式的python檔案中，同路徑下要有這個logger.py檔案
+接下來在python檔案中引入logger.py檔案；
+$ from logger import create_logger
+再來在 if __name__ == '__main__': 測試區塊中；
+$ logger = create_logger("資料夾名稱")
+會在logs資料夾下再建立一個"資料夾名稱"的資料夾，生成的logs檔案會存放於此
+"""
+
 
 dir_path = './logs/' # 設定 logs 目錄
 filename = "{:%Y-%m-%d}".format(datetime.now()) + '.log' # 設定檔名
@@ -9,7 +19,7 @@ filename = "{:%Y-%m-%d}".format(datetime.now()) + '.log' # 設定檔名
 def create_logger(log_folder):
     # config
     logging.captureWarnings(True)  # 捕捉 py waring message
-    logger = logging.getLogger('Jasper')  # 捕捉 py waring message
+    logger = logging.getLogger('Jasper')  # 設定logging名稱，可以自己改成要用的名稱
     logger.setLevel(logging.DEBUG) #設定層級
     detail_formatter = logging.Formatter("%(asctime)s-%(name)s-%(filename)s-%(funcName)s-"
                                          "%(module)s-%(lineno)d-%(levelname)s-%(message)s",
