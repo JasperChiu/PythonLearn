b42edf2dc1976362856bb600f2e9a929206dbdd3
JasperChiu <Jasper.Chiu@beltom.com.tw>
2022-03-31 09:59:51 +0800

    註解掉一行沒用的程式碼

--- model/img_process.py
+++ model/img_process.py
@@ -16,7 +16,7 @@ class ImgProcess:
             mask = np.zeros(img.shape, np.uint8)  # 先建立全黑的遮罩(也可以全白啦，思路相同)
             for i in contours_list:
                 mask = cv2.drawContours(mask, contours, i, (255, 255, 255), -1)
-            dst = cv2.drawContours(img, contours, -1, (255, 0, 0), 3)
+            # dst = cv2.drawContours(img, contours, -1, (255, 0, 0), 3)
             mask_reverse = (np.ones(img.shape, dtype=np.uint8) * 255) - mask
             mask_reverse = cv2.resize(mask_reverse, (origin_img.shape[1], origin_img.shape[0]))  # 遮罩resize回原圖尺寸大小
             img_add = cv2.add(origin_img, mask_reverse)
